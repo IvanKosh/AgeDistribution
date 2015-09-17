26bddbe836f9b765623bf61b75601d3296fdf4b0
Batiatari Kaze <batiatari.dono@gmail.com>
2015-09-17 16:54:50 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -11,8 +11,37 @@ using namespace std;
 int main(int argc, char *argv[]) {
     ifstream stream(argv[1]);
     short n;
-    while (!endf(stream >> n)) {
-        cout << n << endl;
+	
+    stream >> n;
+    while (!stream.eof()) {
+		if (n < 0 || n > 100) {
+			cout << "This program is for humans\n";
+		}
+		else if (n > 65) {
+			cout << "The Golden Years\n";
+		}
+		else if (n > 22) {
+			cout << "Working for the man\n";
+		}
+		else if (n > 18) {
+			cout << "College\n";
+		}
+		else if (n > 14) {
+			cout << "High school\n";
+		}
+		else if (n > 11) {
+			cout << "Middle school\n";
+		}
+		else if (n > 4) {
+			cout << "Elementary school\n";
+		}
+		else if (n > 2) {
+			cout << "Preschool Maniac\n";
+		}
+		else {
+			cout << "Still in Mama's arms\n";
+		}
+        stream >> n;
     }
     return 0;
 }
